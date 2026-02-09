1eeda8612a8df7ad7bf5c4dff5a41c799c2a9ab6
RAHULSRIVASTAVA123 <97719882+RAHULSRIVASTAVA123@users.noreply.github.com>
2026-02-09 21:03:06 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -129,7 +129,7 @@
 
 import { useNavigate } from "react-router-dom";
 import { videos } from "../data/videos";
-import type { Video } from "../types/video";
+// import type { Video } from "../types/video";
 import VideoCard from "../components/VideoCard";
 import "../styles/home.css";
 import { useState, useMemo } from "react";
